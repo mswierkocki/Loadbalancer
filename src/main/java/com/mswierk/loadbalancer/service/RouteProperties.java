d7efde4fd07ed1f846e0e5be92172849c1b3487b
Gargeliach <gargeliach@gmail.com>
2017-10-08 22:12:32 +0200

    Adding resources with config

--- src/main/java/com/mswierk/loadbalancer/service/RouteProperties.java
+++ src/main/java/com/mswierk/loadbalancer/service/RouteProperties.java
@@ -8,9 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
-//
-//
-//@ConfigurationProperties(prefix="my")
 @Component
 @Configuration
 @PropertySource("classpath:config.properties")
